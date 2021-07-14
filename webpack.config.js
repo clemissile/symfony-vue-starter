385f42e3ea9a14ff9fdc29155f53e025936a47f3
clemissile <ganivetclement@gmail.com>
2021-07-14 17:22:56 +0200

    :wrench: wip on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 let Encore = require('@symfony/webpack-encore');
 
 Encore
+    // directory where all compiled assets will be stored
     .setOutputPath('public/build/')
+
+    // what's the public path to this directory (relative to your project's document root dir)
     .setPublicPath('/build')
+
+    // empty the outputPath dir before each build
     .cleanupOutputBeforeBuild()
+
     .enableSourceMaps(!Encore.isProduction())
+
+     // will output as public/build/app.js
     .addEntry('app', './assets/js/main.js')
+    
     .enableSassLoader(function (sassOptions) {
     }, {
         resolveUrlLoader: false,
@@ -13,4 +22,11 @@ Encore
     .enableVueLoader()
     .enableSingleRuntimeChunk();
 
-module.exports = Encore.getWebpackConfig();
+// Use polling instead of inotify
+const config = Encore.getWebpackConfig();
+config.watchOptions = {
+    poll: true,
+};
+
+// Export the final configuration
+module.exports = config;
